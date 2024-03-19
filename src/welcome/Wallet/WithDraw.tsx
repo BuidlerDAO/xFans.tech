14f15638c60220e2ab33236da959c5b6acc8a0a4
Dendi <pingqian1991@gmail.com>
2024-03-19 20:23:01 +0800

    fix: remove useless code

--- src/welcome/Wallet/WithDraw.tsx
+++ src/welcome/Wallet/WithDraw.tsx
@@ -47,25 +47,6 @@ const TextField = styled(MTextField)({
   },
 });
 
-const Left = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-    <path
-      d="M3.68799 8H12.438"
-      stroke="#2E2E32"
-      strokeWidth="1.25"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M7.37499 11.75L3.625 8L7.37499 4.25"
-      stroke="#2E2E32"
-      strokeWidth="1.25"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
-
 const WithDraw = () => {
   const [isOpen, { setLeft: close, setRight: open }] = useToggle(false);
   const { balance, accounts } = useGlobalUserStore((state) => ({
